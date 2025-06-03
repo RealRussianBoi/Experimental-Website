84a95dc0119a6afbf9f367a37c7ca5b3c4b1f7de
RealRussianBoi <dekingdomcrafter@gmail.com>
2025-06-03 17:18:18 -0400

    Finished assignment 5-1

--- travlr/app.js
+++ travlr/app.js
@@ -8,6 +8,11 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
+var apiRouter = require('./app_api/routes/index');
+
+
+//Bring in the Database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -28,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
