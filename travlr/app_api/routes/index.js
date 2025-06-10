e8f66aa7ee555a468af4e107f0c0f98661f23499
RealRussianBoi <dekingdomcrafter@gmail.com>
2025-06-09 21:42:06 -0400

    Completed module 6

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
+router
+    .route("/trips")
+    .get(tripsController.tripsList) //GET Method routes tripList.
+    .post(tripsController.tripsAddTrip); //POST Method adds a trip.
 
-//Get method routes tripFindByCode - requires parameter
+//GET method routes tripFindByCode - requires parameter
+//PUT method routes tripsUpdateTrip - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
